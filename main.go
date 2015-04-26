ae55a9bc333476a14f71c4905c7dae114b23e308
Sebastian Jug <sebastian.jug@gmail.com>
2015-04-25 20:15:30 -0400

    Second file for other operation

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -9,16 +8,11 @@ import (
 	"github.com/sjug/am-go/server"
 )
 
-func userTierHandler(w http.ResponseWriter, r *http.Request, ps map[string]string) {
-	// TODO userTierHandler
-	fmt.Fprintf(w, "hello, %s!\n", ps["num"])
-}
-
 func main() {
 	const port = ":8080"
 	router := httptreemux.New()
 	server.InitDetails(router)
-	router.GET("/user/tier/:num", userTierHandler)
+	//server.InitTier(router)
 
 	log.Println("Listening on port", port)
 	log.Fatal(http.ListenAndServe(port, router))
