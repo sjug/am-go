95db8da9fa6ac68520046b5af0b5724f90441821
Sebastian Jug <sebastian.jug@gmail.com>
2015-04-25 21:26:54 -0400

    Database cleaned up, needs testing

--- database/select.go
+++ database/select.go
@@ -1,25 +1,31 @@
 package database
 
-import _ "github.com/go-sql-driver/mysql"
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 const stmtSelectUserByNumber = "SELECT name, cash, dream FROM users WHERE number = ?"
 
-type user struct {
+// User database structure
+type User struct {
 	name         string
 	dreamBalance int
 	cashBalance  int
 }
 
 // SelectUserByNumber function selects user info via collector number
-func SelectUserByNumber(num int) (user, error) {
+func SelectUserByNumber(num int) (*User, error) {
+	tempUser := User{}
 	stmt, err := db.Prepare(stmtSelectUserByNumber)
 	if err != nil {
-		return &user, err
+		return &tempUser, err
 	}
+	defer stmt.Close()
 
-	err = stmt.QueryRow(1).Scan(&user)
+	err = stmt.QueryRow(1).Scan(&tempUser)
 	if err != nil {
-		return &user, err
+		return &tempUser, err
 	}
-	return &user, nil
+	return &tempUser, nil
 }
