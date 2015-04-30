32e5517d6883c4fe561ea3e24c63ecb5805f6bf9
Sebastian Jug <sjug@loyalty.com>
2015-04-30 13:42:25 -0400

    Slight changes to files hosting

--- server/files.go
+++ server/files.go
@@ -8,21 +8,18 @@ import (
 )
 
 func pageHandler(w http.ResponseWriter, r *http.Request, ps map[string]string) {
-	//file := "content/" + ps["page"]
-	//t, _ := template.ParseFiles(file)
-	//t.Execute(w, nil)
 	http.ServeFile(w, r, filepath.Join("content/", ps["file"]))
 	return
 }
 
-func staticHandler(w http.ResponseWriter, r *http.Request, ps map[string]string) {
-	http.ServeFile(w, r, filepath.Join("content/", ps["file"]))
+func fileHandler(w http.ResponseWriter, r *http.Request, ps map[string]string) {
+	http.ServeFile(w, r, filepath.Join("content/", ps["filepath"]))
 	return
 }
 
 // InitStatic sets up routing for static webpages
 func InitStatic(router *httptreemux.TreeMux) {
 	router.GET("/", pageHandler)
-	router.GET("/:file", staticHandler)
+	router.GET("/*filepath", fileHandler)
 
 }
