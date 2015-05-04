e3fa755d28e4b0fe9af2c0f97161f2e68a327d2f
Sebastian Jug <sjug@loyalty.com>
2015-05-04 15:34:44 -0400

    Trying to mock soap request

--- soap/soap_test.go
+++ soap/soap_test.go
@@ -2,6 +2,11 @@ package soap
 
 import (
 	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
 
@@ -168,4 +173,24 @@ func TestParseXML(t *testing.T) {
 		})
 	})
 
+}
+
+func TestCallSoap(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, client")
+	}))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	greeting, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", greeting)
 }
